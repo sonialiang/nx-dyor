c2d55e0c66ac8bdbf666b9d24a365c254b664472
sonialiang <bingo605@gmail.com>
2022-08-23 01:41:23 +0900

    chore: change locale path

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = {
   i18n: {
     defaultLocale: 'ja',
     locales: ['en', 'ja'],
   },
+  localePath: path.resolve('./locales'),
   reloadOnPrerender: process.env.NODE_ENV === 'development',
 };
